3305941dca6366dac630cf457fbf3ade585efc5d
Karem Elzftawy <karem.elzftawy@gmail.com>
2017-09-02 15:56:35 +0200

    Add files via upload

---
+++ Lora_with_feather_and_Arduino_Mega/Readme.md
@@ -0,0 +1,2 @@
+Adafruit RFM69HCW and RFM9X LoRa Packet Radio Breakouts Tutorial: https://learn.adafruit.com/adafruit-rfm69hcw-and-rfm96-rfm95-rfm98-lora-packet-padio-breakouts?view=all <br />
+Adafruit Feather 32u4 Radio with RFM69HCW Module Tutorial: https://learn.adafruit.com/adafruit-feather-32u4-radio-with-rfm69hcw-module?view=all#downloads <br />
---
+++ MQ2_With_Adafruits_Feather/Arduino_Sketch/MQ-2_With_Feather/MQ-2_With_Feather.ino
@@ -0,0 +1,27 @@
+const int gasPin = A5; //GAS sensor output pin to Feather analog A0 pin
+// Your threshold value
+int sensorThres = 400;
+int redLed = 13;
+
+void setup()
+{
+  pinMode(redLed, OUTPUT);
+  pinMode(gasPin, INPUT);
+  Serial.begin(9600); //Initialize serial port - 9600 bps
+}
+
+void loop()
+{
+  int analogSensor = analogRead(gasPin);
+  Serial.println(analogSensor);
+   // Checks if it has reached the threshold value
+  if (analogSensor > sensorThres)
+  {
+    digitalWrite(redLed, HIGH);
+  }
+  else
+  {
+    digitalWrite(redLed, LOW);
+  }
+  delay(1000); // Print value every 1 sec.
+}
